1889a19077ad219367738d89f55730e1ff570c7c
dengyu <313187515@qq.com>
2020-04-14 21:36:19 +0800

    解决事件方法this指向问题

--- react-demo/src/Hello.jsx
+++ react-demo/src/Hello.jsx
@@ -1,9 +1,26 @@
-import React,{Component} from 'react'
+import React, { Component } from 'react'
 
 export default class Hello extends Component {
-  render () {
+  constructor(){
+    super()
+    this.state = {
+      count: 0
+    }
+  }
+  hh() {
+    console.log('点击事件触发了');
+  }
+  countAdd(){
+    this.setState({
+      count: ++this.state.count
+    })
+  }
+  render() {
     return (
-      <div>把组件抽离到jsx中</div>
+      <div>
+      <div onClick={this.hh}>把组件抽离到jsx中</div>
+    <button onClick={()=>this.countAdd()}>count的值是：{this.state.count}</button>
+      </div>
     )
   }
 }
