11a9f0883451db00e196369e2066fd902db9ed0c
dengyu <313187515@qq.com>
2020-04-15 21:52:44 +0800

    兄弟传值

--- react-demo/src/Hello.jsx
+++ react-demo/src/Hello.jsx
@@ -1,35 +1,45 @@
-import React, { createRef } from 'react'
+import React, { createRef, createContext } from 'react'
+// import { render } from 'react-dom'
+const {Provider,Consumer} = createContext()
 
-export default class Hello extends React.Component {
-  constructor() {
-    super()
-    this.state = {
-      value: ''
-    }
-  }
 
-  transValue(text) {
-    this.setState({
-      value: text
-    })
-  }
-
+const Child1 = (props) => {
+  return (
+    // <Consumer>
+  <div>这里是兄弟给我说的话{props.value}
+  {/* {
+  data => <h1>{data}</h1>
+  } */}
+  </div>
+  // </Consumer>
+  )
+}
+class Child2 extends React.Component {
   render() {
     return (
+      <button onClick={() => this.props.saySomething('兄弟你好！')}>给兄弟说句话</button>
+    )
+  }
+}
+export default class Hello extends React.Component {
+  state = {
+    value: ''
+  }
+  saySomething(value) {
+    this.setState({
+      value: value
+    })
+  }
+  render() {
+    return (
+      // <Provider value="666">
       <div>
-        <Child transValue={this.transValue.bind(this)} />
-        这是子组件传过来的：{this.state.value}
+        {/* <Child2 saySomething={this.saySomething.bind(this)} /> */}
+        <Child2 saySomething={(value)=>this.saySomething(value)} />
+        <Child1 value={this.state.value} hh={this.state.value} />
       </div>
-
+      // </Provider>
     )
   }
 }
 
-function Child(props){
-  return (
-    <div>
-      <h3>我是子组件</h3>
-      <button onClick={() => props.transValue('我是子组件传过去的')}>传递一个值给父组件</button>
-    </div>
-  )
-}
