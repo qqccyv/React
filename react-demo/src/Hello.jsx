de02021b2a07ba67eb112316ed9d9dd26b28a4cb
dengyu <313187515@qq.com>
2020-04-15 19:05:36 +0800

    简单评论功能的实现

--- react-demo/src/Hello.jsx
+++ react-demo/src/Hello.jsx
@@ -1,33 +1,61 @@
-import React, { Component } from 'react'
+import React from 'react'
 
-export default class Hello extends Component {
-  constructor(){
-    super()
-    this.state = {
-      count: 0,
-      text: '随便写点什么'
-    }
+export default class Hello extends React.Component {
+
+  state = {
+    comments: [
+    //   {
+    //   id: 1,
+    //   name: 'jack',
+    //   content: 'haha'
+    // }
+  ],
+    name: '',
+    content: ''
   }
-  hh() {
-    console.log('点击事件触发了');
-  }
-  countAdd(){
+
+  valueChange({ target: { value, name } }) {
     this.setState({
-      count: ++this.state.count
+      [name]: value
     })
+    // console.log(this.state);
+
   }
-  changeValue(e){
-    this.setState({
-      text: e.target.value
-    })
+
+  addComment() {
+
+    let { comments, name, content } = this.state
+    name.trim() && content.trim() ? this.setState({
+      comments: [{
+        id: comments[0]? comments[0].id+1: 1,
+        name: name,
+        content, content
+      }, ...this.state.comments]
+    },()=>this.setState({name: '',content: ''})) : alert('请输入评论人和评论内容')
   }
+
+  renderComments(){
+    return this.state.comments.length === 0 ? <div>暂时还没有人评论！快来说说你的看法吧！</div> : this.state.comments.map(item => (<li key={item.id}><h3>{item.name}</h3><div>{item.content}</div></li>))
+  }
+
   render() {
     return (
       <div>
-      <div onClick={this.hh}>把组件抽离到jsx中</div>
-    <button onClick={()=>this.countAdd()}>count的值是：{this.state.count}</button>
-    <input type="text" value={this.state.text} onChange={this.changeValue.bind(this)} />
-    <div>{this.state.text}</div>
+        <div>
+          评论人：<input type="text" name="name" id="" placeholder='请输入评论人' value={this.state.name} onChange={this.valueChange.bind(this)} /> <br />
+        </div>
+        <div>
+          评论内容：<textarea name="content" id="" cols="30" rows="10" placeholder="请输入评论内容" value={this.state.content} onChange={this.valueChange.bind(this)}></textarea> <br />
+        </div>
+        <div>
+          <button onClick={this.addComment.bind(this)}>发表评论</button>
+        </div>
+        <div>
+          <h3>评论列表：</h3>
+          <ul>
+            {this.renderComments()}
+          </ul>
+        </div>
       </div>
     )
   }
