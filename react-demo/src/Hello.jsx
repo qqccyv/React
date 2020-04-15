c707740e5ef0c46daa0c08a3aad6f0db05628719
dengyu <313187515@qq.com>
2020-04-15 10:31:48 +0800

    表单处理

--- react-demo/src/Hello.jsx
+++ react-demo/src/Hello.jsx
@@ -4,7 +4,8 @@ export default class Hello extends Component {
   constructor(){
     super()
     this.state = {
-      count: 0
+      count: 0,
+      text: '随便写点什么'
     }
   }
   hh() {
@@ -15,11 +16,18 @@ export default class Hello extends Component {
       count: ++this.state.count
     })
   }
+  changeValue(e){
+    this.setState({
+      text: e.target.value
+    })
+  }
   render() {
     return (
       <div>
       <div onClick={this.hh}>把组件抽离到jsx中</div>
     <button onClick={()=>this.countAdd()}>count的值是：{this.state.count}</button>
+    <input type="text" value={this.state.text} onChange={this.changeValue.bind(this)} />
+    <div>{this.state.text}</div>
       </div>
     )
   }
