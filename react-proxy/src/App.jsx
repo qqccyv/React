02c27dfc57a6b5b792df9a1160cd2c5cd95cf002
qqccyv <313187515@qq.com>
2021-04-14 21:43:39 +0800

    git搜索案例-proxy

--- react-proxy/src/App.jsx
+++ react-proxy/src/App.jsx
@@ -5,19 +5,30 @@ import Search from './components/Search/Search';
 import './App.css';
 import axios from 'axios'
 export default class App extends Component {
+  state = {
+    users: [],
+    isFirst: true,
+    loading: false,
+    err: ''
+  }
+  updateStateStatus = (statusObj) => {
+    this.setState(statusObj)
+  }
   search = (keyWord) => {
-    console.log(keyWord);
+    this.updateStateStatus({ isFirst: false, loading: true })
     axios.get(`/api1/search/users?q=${keyWord}`).then(res => {
-      console.log(res);
+      // console.log(res);
+      this.updateStateStatus({ users: res.data.items, loading: false })
     }, error => {
       console.error(error);
+      this.updateStateStatus({ err: error.message, loading: false })
     })
   }
   render() {
     return (
       <div className="container">
         <Search search={this.search}></Search>
-        <List></List>
+        <List {...this.state}></List>
       </div>
     );
   }
--- react-proxy/src/components/List/List.jsx
+++ react-proxy/src/components/List/List.jsx
@@ -1,39 +1,32 @@
 import React, { Component } from 'react'
-
+import PropTypes from 'prop-types'
 export default class List extends Component {
+  static propTypes = {
+    users: PropTypes.array.isRequired,
+    isFirst: PropTypes.bool.isRequired,
+    loading: PropTypes.bool.isRequired,
+    err: PropTypes.string.isRequired
+  }
   render() {
+    const { users, isFirst, loading, err } = this.props
     return (
       <div className="row">
-        <div className="card">
-          <a href="https://github.com/reactjs" target="_blank" rel="noreferrer">
-            <img src="https://avatars.githubusercontent.com/u/6412038?v=3" style={{ width: '100px' }} alt="avatar" />
-          </a>
-          <p className="card-text">reactjs</p>
-        </div>
-        <div className="card">
-          <a href="https://github.com/reactjs" target="_blank" rel="noreferrer">
-            <img src="https://avatars.githubusercontent.com/u/6412038?v=3" style={{ width: '100px' }} alt="avatar" />
-          </a>
-          <p className="card-text">reactjs</p>
-        </div>
-        <div className="card">
-          <a href="https://github.com/reactjs" target="_blank" rel="noreferrer">
-            <img src="https://avatars.githubusercontent.com/u/6412038?v=3" style={{ width: '100px' }} alt="avatar" />
-          </a>
-          <p className="card-text">reactjs</p>
-        </div>
-        <div className="card">
-          <a href="https://github.com/reactjs" target="_blank" rel="noreferrer">
-            <img src="https://avatars.githubusercontent.com/u/6412038?v=3" style={{ width: '100px' }} alt="avatar" />
-          </a>
-          <p className="card-text">reactjs</p>
-        </div>
-        <div className="card">
-          <a href="https://github.com/reactjs" target="_blank" rel="noreferrer">
-            <img src="https://avatars.githubusercontent.com/u/6412038?v=3" style={{ width: '100px' }} alt="avatar" />
-          </a>
-          <p className="card-text">reactjs</p>
-        </div>
+        {
+          isFirst ? <h2>输入关键字，点击搜索</h2> :
+            loading ? <h2>loading..</h2> :
+              err ? <h2>err</h2> :
+                users.map(userObj => {
+                  return (
+                    <div key={userObj.id} className="card">
+                      <a href={userObj.html_url} target="_blank" rel="noreferrer">
+                        <img src={userObj.avatar_url} style={{ width: '100px' }} alt="avatar" />
+                      </a>
+                      <p className="card-text">{userObj.login}</p>
+                    </div>
+                  )
+                })
+        }
+
       </div>
     )
   }
