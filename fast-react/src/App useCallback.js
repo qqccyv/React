842fc38b90170850378cc49c2f76c197b2a4769a
dengyu <dengyu@qq.com>
2022-07-27 18:04:10 +0800

    useCallback useMemo

--- fast-react/src/App useCallback.js
+++ fast-react/src/App useCallback.js
@@ -1,4 +1,4 @@
-import React, { memo, useMemo, useState } from 'react'
+import React, { memo, useCallback, useState } from 'react'
 // memo 主要用于缓存纯静态组件
 
 const Father = memo((props) => {
@@ -13,11 +13,9 @@ const Father = memo((props) => {
 
 export default function App() {
   const [num, setNum] = useState(1)
-  // useMemo和 useCallback 使用方法差不多，区别就是不知直接调用，需要再返回一个函数，  典型的高阶函数
-  const add = useMemo(
-    () => {
-      return () => { setNum((num) => { return num + 1 }) }
-    },
+  // useCallback 配合memo使用，达到缓存含有动态方法的子组件的作用，但是在赋值等操作时，需要注意拿取最新的状态数据
+  const add = useCallback(
+    () => { setNum((num) => { return num + 1 }) },
     [],
   )
 
