7b70ab69f66e17efe44a3119a8b86f71f18fa33c
dengyu <dengyu@qq.com>
2022-07-29 10:20:06 +0800

    redux—thunk

--- fast-react/src/App.js
+++ fast-react/src/App.js
@@ -1,7 +1,8 @@
 import React, { /* memo, useMemo, useState */ } from 'react'
 // memo 主要用于缓存纯静态组件
-import { createStore } from 'redux'
+import { createStore, applyMiddleware } from 'redux'
 import { Provider, connect } from 'react-redux'
+import thunk from 'redux-thunk'
 const addAction = (num) => {
   return {
     type: 'ADD',
@@ -13,21 +14,38 @@ const squareAction = () => {
     type: 'SQUARE'
   }
 }
+const getAction = () => {
+  return (dispatch, getState) => {
+    // 请求路径由于是在publick文件夹下，所以是基于根路径
+    fetch('./data.json')
+      .then(res => {
+        return res.json()
+      })
+      .then(res => {
+        dispatch({
+          type: 'GET',
+          num: res.num
+        })
+      })
+  }
+}
 const reducer = (state = 0, action) => {
   switch (action.type) {
     case 'ADD':
       return state + action.num
     case 'SQUARE':
       return state * state
+    case 'GET':
+      return action.num
     default:
       return state
   }
 
 }
-const store = createStore(reducer);
+const store = createStore(reducer, applyMiddleware(thunk));
 
 const Container = (props) => {
-  const { add, square, num } = props
+  const { add, square, num, get } = props
   return (
     <>
       <h1>{num}</h1>
@@ -40,6 +58,9 @@ const Container = (props) => {
       <button onClick={() => {
         square()
       }}>平方</button>
+      <button onClick={() => {
+        get()
+      }}>请求</button>
     </>
   )
 }
@@ -53,7 +74,8 @@ const mapSateToProps = (state) => {
 const mapDiapatchToProps = (dispatch) => {
   return {
     add: (value) => { dispatch(addAction(value)) },
-    square: () => { dispatch(squareAction()) }
+    square: () => { dispatch(squareAction()) },
+    get: () => { dispatch(getAction()) }
   }
 }
 // 通过react-redux提供的connect方法，将redux的state，dispatch和action映射到子容器的props上，然后包裹子容器触发视图更新
